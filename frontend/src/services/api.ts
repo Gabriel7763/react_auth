52b4046fdcf01764473ae661d27edbb9cb02b049
Gabriel Nunes <gabrielconceicaonunes@gmail.com>
2025-02-19 09:53:39 -0300

    Atualizando para deploy na Vercel

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 const api = axios.create({
-  baseURL: "http://localhost:3000/auth",
+  baseURL: "https://react-auth-zkk9.onrender.com/auth",
   withCredentials: true,
 });
 
